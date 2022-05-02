ad1cf780a3ec0b139ccba09aedcc71cdd3fcf579
Sugarcothe <ifeanyivalentine82@gmail>
2022-05-02 17:19:41 +0100

    feautures fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                     <li className={styles.listItem}>Homepage</li>
                     <li className={styles.listItem}>Products</li>
                     <li className={styles.listItem}>Menu</li>
-                    <Image src='/img/noko.png' alt='' width='160px' height='160px'/>
+                    <Image src='/img/noko.png' alt='' width='400px' height='160px'/>
                     <li className={styles.listItem}>Events</li>
                     <li className={styles.listItem}>Blog</li>
                     <li className={styles.listItem}>Contact</li>
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Featured from '../components/Featured'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     
       <Head>
-        <title>Native Pot</title>
+        <title>Mr Jellof</title>
         <meta name="description" content="Best local pot" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Content
+      <Featured/>
      
       
     </div>
